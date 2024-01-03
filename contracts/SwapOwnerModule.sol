ca38c450adf8197bfe9f9c5c87bb4ac2028fe20d
Fbartoli <florent.bartoli@sygnum.com>
2024-01-03 09:48:42 +0100

    Lint

--- contracts/SwapOwnerModule.sol
+++ contracts/SwapOwnerModule.sol
@@ -12,23 +12,18 @@ interface IOwnerManager {
 }
 
 contract SwapOwnerModule is Module {
-
-    /// @param _owner Address of the owner
-    /// @param _avatar Address of the avatar (e.g. a Gnosis Safe)
-    /// @param _target Address of the contract that will call exec function
+    /// @param avatar Address of the avatar (e.g. a Gnosis Safe) Avatars must expose an interface like IAvatar.sol.
+    /// @param target Address of the contract that will call exec function
+    /// @param owner Address of the owner
     constructor(address target, address avatar, address owner) {
-        bytes memory initParams = abi.encode(
-            target,
-            avatar,
-            owner
-        );
+        bytes memory initParams = abi.encode(target, avatar, owner);
         setUp(initParams);
     }
 
     function setUp(bytes memory initializeParams) public override initializer {
         (address target, address avatar, address owner) = abi.decode(
             initializeParams,
-            (address,address, address)
+            (address, address, address)
         );
         __Ownable_init();
         setAvatar(avatar);
@@ -36,25 +31,31 @@ contract SwapOwnerModule is Module {
         transferOwnership(owner);
     }
 
-    function startRecovery(address prevOwner,address oldOwners, address newOwners) external onlyOwner {
-        require(_swapOwner(prevOwner, oldOwners, newOwners), 'Module transaction failed');
+    function startRecovery(
+        address prevOwner,
+        address oldOwners,
+        address newOwners
+    ) external onlyOwner {
+        require(
+            _swapOwner(prevOwner, oldOwners, newOwners),
+            "Module transaction failed"
+        );
     }
-
-
 
     function _swapOwner(
         address prevOwner,
         address oldOwner,
         address newOwner
     ) internal returns (bool) {
-        return exec(
-            target,
-            0,
-            abi.encodeCall(
-                IOwnerManager.swapOwner,
-                (prevOwner, oldOwner, newOwner)
-            ),
-            Enum.Operation.Call
-        );
+        return
+            exec(
+                target,
+                0,
+                abi.encodeCall(
+                    IOwnerManager.swapOwner,
+                    (prevOwner, oldOwner, newOwner)
+                ),
+                Enum.Operation.Call
+            );
     }
 }
